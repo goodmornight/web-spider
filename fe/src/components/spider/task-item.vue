31a3b04e3420ff308386acad905b0fac761c6c29
GoodNight <goodnightlilanhui@gmail.com>
2020-11-24 23:44:22 +0800

    创建task-item

---
+++ fe/src/components/spider/task-item.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  props:{
+    taskId: {
+      type: String,
+      default: '12345',
+    },
+    domain: {
+      type: String,
+      default: 'vimeo',
+    },
+    type: {
+      type: String,
+      default: 'video',
+    },
+    url: {
+      type: String,
+      default: 'https://127.0.0.1',
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    createTime: {
+      type: Number,
+      default: 1606120037,
+    },
+    updateTime: {
+      type: Number,
+      default: 1706120037,
+    },
+  }
+}
+</script>
+<template>
+  <div class="task-item col-12">
+    <div class="col-2">{{ taskId }}</div>
+    <div class="col-1">{{ domain }}</div>
+    <div class="col-1">{{ type }}</div>
+    <div class="col-2">{{ url }}</div>
+    <div class="col-2">
+      <span class="badge"
+      :class="{
+        'badge-soft-success': isCompleted,
+        'badge-soft-warning': !isCompleted
+      }"
+      >
+        {{ isCompleted ? '完成' : '未完成' }}
+      </span>
+    </div>
+    <div class="col-2 time">
+      {{ createTime | moment('YY/MM/DD HH:mm:ss') }}
+    </div>
+    <div class="col-2 time">
+      {{ updateTime | moment('YY/MM/DD HH:mm:ss') }}
+    </div>
+  </div>	
+</template>
+<style type="text/css">
+.task-item {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  min-width: 0;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 0 solid rgba(0, 0, 0, 0.125);
+  box-shadow: 0 0.05rem 0.01rem rgba(75, 75, 90, 0.075);
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+}
+.time {
+  color: #43d39e;
+}
+</style>
--- fe/src/router/views/pages/spider/tasks-controller.vue
+++ fe/src/router/views/pages/spider/tasks-controller.vue
@@ -1,34 +1,19 @@
 <script>
 import appConfig from '@src/app.config'
 import Layout from '@layouts/main'
-import PageHeader from '@components/page-header'
+import TaskItem from '@components/spider/task-item'
 
 /**
- * Starter component
+ * TasksController component
  */
 export default {
   page: {
-    title: 'Starter',
-    meta: [{ name: 'description', content: appConfig.description }],
+    title: 'Tasks'
   },
-  components: { Layout, PageHeader },
+  components: { Layout, TaskItem },
   data() {
     return {
-      title: 'Starter',
-      items: [
-        {
-          text: 'Shreyu',
-          href: '/',
-        },
-        {
-          text: 'Pages',
-          href: '/',
-        },
-        {
-          text: 'Starter',
-          active: true,
-        },
-      ],
+      
     }
   },
 }
@@ -36,6 +21,27 @@ export default {
 
 <template>
   <Layout>
-    <PageHeader :title="title" :items="items" />
+    <div class="row mt-2">
+      <b-form-input
+        class="col-xl-12 col-lg-12 col-md-12 col-sm-12"
+        value="Some text value..."
+      ></b-form-input>
+    </div>
+    <div class="row mt-4">
+      <span class="col-2">任务ID</span>
+      <span class="col-1">来源</span>
+      <span class="col-1">类型</span>
+      <span class="col-2">URL</span>
+      <span class="col-2">状态</span>
+      <span class="col-2">创建时间</span>
+      <span class="col-2">更新时间</span>
+    </div>
+    <div class="row">
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+    </div>
+    
   </Layout>
 </template>
