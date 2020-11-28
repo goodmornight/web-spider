5368f7c2b00b6030a8f8e34f6598b682edaa1603
GoodNight <goodnightlilanhui@gmail.com>
2020-11-28 22:00:46 +0800

    修改完成图表

--- fe/src/router/views/pages/spider/search.vue
+++ fe/src/router/views/pages/spider/search.vue
@@ -37,7 +37,7 @@ export default {
         chart: {
           type: 'bar',
           width: 100,
-          height: 900,
+          height: 80,
           // height: 350,
           // offsetX: -10,
           // offsetY: -5,
